a94b7fd373ff4acfd30cb61a68254dee0e49bf45
Taylor Carroll <taylorcarroll@Taylors-MacBook-Pro.local>
2019-08-29 10:25:40 -0500

    refactored to accept one argument vs 4

--- src/script/main.js
+++ src/script/main.js
@@ -74,62 +74,31 @@ const students = [
 ]
 
 
-//creates a student
-const createStudentComponent = (name, subject, info, grade) => {
-    return `
-        <div class="student">
-            <h1 class="${grade}">${name}</h1>
-            <section class= "bordered dashed section--padded">${subject}</section>
-            <aside class="pushRight">${info}</aside>
-        </div>
-    `;
-};
+// //creates a student
+// const createStudentComponent = (name, subject, info, grade) => {
+//     return `
+//         <div class="student">
+//             <h1 class="xx- large ${grade}">${name}</h1>
+//             <section class= "bordered dashed section--padded">${subject}</section>
+//             <aside class="pushRight">${info}</aside>
+//         </div>
+//     `;
+// };
 
-// Then store a reference to an existing HTML element
-const studentContainer = document.querySelector("#container")
+// // Then store a reference to an existing HTML element
+// const studentContainer = document.querySelector("#container")
 
-//position the students on the DOM
-for (let i = 0; i < students.length; i++) {
-    const student = students[i];
-    //changes student class based on score
-    let studentComponent;
-    if (student.score >= 60) {
-        studentComponent = "passing";
-    } else {
-        studentComponent = "failing";
-    }
-    // Update its contents with the return value of the function
-    studentContainer.innerHTML += createStudentComponent(
-        student.name,
-        student.subject,
-        student.info,
-        studentComponent
-    );
-}
-
-
-// const passing = {
-//     <div class="student">
-//     <h1 class="xx-large passing">Student Name</h1>
-//     <section class="bordered dashed section--padded">Subject</section>
-//     <aside class="pushRight">Additional information</aside>
-// </div>
-// }
-
-// const failing = {
-
-// }
-
-// const studentContainer = document.querySelector("#container");
-
+// //position the students on the DOM
 // for (let i = 0; i < students.length; i++) {
 //     const student = students[i];
+//     //changes student class based on score
 //     let studentComponent;
 //     if (student.score >= 60) {
-//         studentComponent = (student)passing
+//         studentComponent = "passing";
 //     } else {
-//         studentComponent = (student)failing
+//         studentComponent = "failing";
 //     }
+//     // Update its contents with the return value of the function
 //     studentContainer.innerHTML += createStudentComponent(
 //         student.name,
 //         student.subject,
@@ -137,5 +106,52 @@ for (let i = 0; i < students.length; i++) {
 //         studentComponent
 //     );
 // }
+
+
+// //creates a student
+// const createStudentComponent = (name, subject, info, grade) => {
+//     return `
+//         <div class="student">
+//             <h1 class="xx- large ${grade}">${name}</h1>
+//             <section class= "bordered dashed section--padded">${subject}</section>
+//             <aside class="pushRight">${info}</aside>
+//         </div>
+//     `;
+// };
+
+//object that defines a student who passed
+const passed = (person) => {
+    return `
+    <div class="student">
+    <h1 class="xx-large passing">${person.name}</h1>
+    <section class="bordered dashed section--padded">${person.subject}</section>
+    <aside class="pushRight">${person.info}</aside>
+</div>`
+}
+
+//object that defines a student who failed
+const failed = (person) => {
+    return `
+    <div class="student">
+    <h1 class="xx-large failing">${person.name}</h1>
+    <section class="bordered dashed section--padded">${person.subject}</section>
+    <aside class="pushRight">${person.info}</aside>
+</div>`
+}
+
+//allows you to place the objects on the DOM
+let studentContainer = document.querySelector("#container");
+
+//function to create students based on score
+for (const student of students) {
+    console.log(student.score); //console to make sure this function was actually doing something
+    let studentComponent = ""
+    if (student.score >= 60) {
+        studentComponent = passed(student)     
+    } else {
+        studentComponent = failed(student)
+    }
+    studentContainer.innerHTML += studentComponent;
+}
 
 
